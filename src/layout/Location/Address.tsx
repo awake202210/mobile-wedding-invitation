56178f0c2aaabf2b0066f5c1540696dd6ef65b19
jkan0405 <jkan0405@naver.com>
2025-06-02 01:24:26 +0900

    주소 텍스트 변경

--- src/layout/Location/Address.tsx
+++ src/layout/Location/Address.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
-import { Caption, PointTitle } from '@/components/Text.tsx';
+import { GiPin } from 'react-icons/gi';
+import { TbArrowBigRightLinesFilled } from 'react-icons/tb';
 import { ILocationInfo } from '@/types/data.ts';
 
 const Address = () => {
@@ -11,8 +12,13 @@ const Address = () => {
         const { title, desc } = item;
         return (
           <Way key={title}>
-            <PointTitle>{title}</PointTitle>
-            <Caption>{desc}</Caption>
+            <WayPointTitle>
+              <GiPin /> {title}
+            </WayPointTitle>
+            <WayCaption>
+              <TbArrowBigRightLinesFilled />
+              {desc}
+            </WayCaption>
           </Way>
         );
       })}
@@ -34,4 +40,26 @@ const Way = styled.div`
   display: flex;
   flex-direction: column;
   text-align: left;
+`;
+
+const WayPointTitle = styled.p`
+  font-family: MaruburiBold, serif;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0.5rem;
+  align-items: center;
+  font-size: 1.2rem;
+  line-height: 2.2rem;
+  margin: 0;
+  color: #e88ca6;
+  white-space: pre-line;
+`;
+
+const WayCaption = styled.p<{ textAlign?: string }>`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0.5rem;
+  align-items: center;
+  text-align: ${(props) => (props.textAlign ? props.textAlign : 'start')};
+  white-space: pre-line;
 `;
